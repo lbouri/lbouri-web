226a14e95b4c788b403585f49ff599a927683b42
lbouri1 <laurent.bouri2@gmail.com>
2024-12-11 00:44:52 +0100

    fix: clean config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
-import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === "production";
-module.exports = {
+
+const nextConfig = {
   assetPrefix: isProd ? '/lbouri-web/' : '',
   basePath: isProd ? '/lbouri-web' : '',
-};
-const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
