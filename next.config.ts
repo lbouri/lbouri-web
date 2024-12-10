b03db91efa150a1729e63afb51d29be794afe5fc
lbouri1 <laurent.bouri2@gmail.com>
2024-12-11 00:36:35 +0100

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
-
+const isProd = process.env.NODE_ENV === "production";
+module.exports = {
+  assetPrefix: isProd ? '/lbouri-web/' : '',
+  basePath: isProd ? '/lbouri-web' : '',
+};
 const nextConfig: NextConfig = {
-  /* config options here */
 };
 
 export default nextConfig;
